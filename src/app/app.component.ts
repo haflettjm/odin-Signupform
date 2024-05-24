04105c9be0bf4d452300ece4ffd5b0d53aec77ea
Jacob Haflett <hafletjm01@gmail.com>
2024-05-24 01:58:36 -0400

    Fixing dumb readme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {PageContentComponent} from "./page-content/page-content.component";
+import {HeroBannerComponent} from "./hero-banner/hero-banner.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PageContentComponent, HeroBannerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
